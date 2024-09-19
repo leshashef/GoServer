2e529d03abf1954c5be23c279e92131c752f67d1
leshashef <lesha-shevchenko2001@mail.ru>
2024-09-19 19:05:55 +0300

    up

---
+++ server.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "About Page")
+	})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Index Page")
+	})
+	fmt.Println("Server is listening...")
+	http.ListenAndServe(":8181", nil)
+}
